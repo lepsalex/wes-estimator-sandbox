35d7c637e61aa0ff7e1edf2743616aef1d8f6ff7
Alex <lepsalex@gmail.com>
2020-02-16 11:19:59 -0500

    async all the things

--- main.py
+++ main.py
@@ -1,55 +1,57 @@
 from service.sheets import Sheet
-from service.wes import getWesRuns, extractRunInfo, startVariableParamsRun
+from service.wes import getWesRuns, startWesRuns, extractRunInfo
 
 # The ID and range of the spreadsheet.
 SPREADSHEET_ID = "1oQBjKw9eJbI1ViDiGOTU3i87tBPnn6TGbhQfs6B1_tA"
 
 # RANGES
-RUN_ID_COL_SHORT = "'AutoAlign'!A2:A7"
-RUN_ID_COL = "'AutoAlign'!A40:A53"
-ALIGN_WRITE_START = "'AutoAlign'!C177"
-RUNS_WRITE_START = "'AutoAlign'!A57"
+RUN_ID_COL_TEST = "'AutoAlign'!A2:A7"
 
-autoAlign = Sheet(SPREADSHEET_ID)
+RUN_ID_COL = "'AutoAlign'!A2:A82"
+ALIGN_WRITE_START = "'AutoAlign'!C2"
+
+RUNS_WRITE_START = "'AutoAlign'!A83"
 
 
-def readAutoAlign():
-    wesRunIds = autoAlign.readColumn(RUN_ID_COL_SHORT)
+def readAutoAlign(readCol):
+    wesRunIds = autoAlign.readColumn(readCol)
     return getWesRuns(wesRunIds)
 
 
 def writeAutoAlign(runs, range):
-    align_tasks = [item for sublist in [
-        extractRunInfo(run) for run in runs] for item in sublist]
+    align_tasks = [item for sublist in [extractRunInfo(run) for run in runs] for item in sublist]
     autoAlign.updateRange(align_tasks, range)
 
 
 def startNewJobs(params, range):
-    newRuns = map(startVariableParamsRun, params_for_test)
-    autoAlign.updateRange(list(newRuns), range)
+    newRuns = startWesRuns(params)
+    autoAlign.updateRange(newRuns, range)
 
 
 params_for_test = [
-    # {"cpus": 2, "nfs": "nfs-1-c1"},
-    # {"cpus": 2, "nfs": "nfs-1-c2"},
-    # {"cpus": 8, "nfs": "nfs-1-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 4, "nfs": "nfs-1-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 6, "nfs": "nfs-2-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 10, "nfs": "nfs-2-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 12, "nfs": "nfs-2-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 14, "nfs": "nfs-2-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 16, "nfs": "nfs-3-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 18, "nfs": "nfs-3-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 20, "nfs": "nfs-3-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 22, "nfs": "nfs-3-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 24, "nfs": "nfs-4-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 28, "nfs": "nfs-4-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 32, "nfs": "nfs-4-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
-    {"cpus": 36, "nfs": "nfs-4-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"}
+    # {"cpus": 2, "nfs": "nfs-1-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 2, "nfs": "nfs-1-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 36, "nfs": "nfs-1-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 4, "nfs": "nfs-1-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 34, "nfs": "nfs-2-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 32, "nfs": "nfs-2-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 30, "nfs": "nfs-2-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 28, "nfs": "nfs-2-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 26, "nfs": "nfs-3-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 24, "nfs": "nfs-3-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 22, "nfs": "nfs-3-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 20, "nfs": "nfs-3-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 18, "nfs": "nfs-4-c1", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 16, "nfs": "nfs-4-c2", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 14, "nfs": "nfs-4-c3", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"},
+    # {"cpus": 12, "nfs": "nfs-4-c4", "analysisId": "15c2fb45-d531-4735-82fb-45d531573575"}
 ]
 
+# Init Spreadsheet
+autoAlign = Sheet(SPREADSHEET_ID)
+
 # Write align data to sheet
-# writeAutoAlign(readAutoAlign(), ALIGN_WRITE_START)
+writeAutoAlign(readAutoAlign(RUN_ID_COL), ALIGN_WRITE_START)
 
 # Run new jobs and record run_id to sheet
-startNewJobs(params_for_test, RUNS_WRITE_START)
+# startNewJobs(params_for_test, RUNS_WRITE_START)
--- service/wes.py
+++ service/wes.py
@@ -1,12 +1,34 @@
 import json
 import requests
 from urllib import request
+import aiohttp
+import asyncio
 
 WES_BASE = "http://wes.flow.infra.icgc-argo.org/api/v1/runs/"
 MIN_MEM = 20
 
 
-def startVariableParamsRun(params, tokenFile="api_token", scoreTokenFile="score_api_token"):
+def getWesRuns(wesIds):
+    loop = asyncio.get_event_loop()
+    coroutines = [getWesRun(wesId) for wesId in wesIds]
+    return loop.run_until_complete(asyncio.gather(*coroutines))
+
+
+async def getWesRun(wesId):
+    async with aiohttp.ClientSession() as session:
+        async with session.get("{}{}".format(WES_BASE, wesId.strip())) as response:
+            data = await response.json()
+            return {
+                "runId": data["run_id"],
+                "params": data["request"]["workflow_params"],
+                "start": data["run_log"]["start_time"],
+                "end": data["run_log"]["end_time"],
+                "duration": data["run_log"]["duration"],
+                "tasks": list(filter(None, map(processTasks, data["task_logs"])))
+            }
+
+
+def startWesRuns(paramsList, tokenFile="api_token", scoreTokenFile="score_api_token"):
     # Get tokens from files
     token_f = open(tokenFile, 'r')
     api_token = token_f.readline().strip()
@@ -16,61 +38,71 @@ def startVariableParamsRun(params, tokenFile="api_token", scoreTokenFile="score_
     score_api_token = score_token_f.readline().strip()
     score_token_f.close()
 
-    cpus = params["cpus"]
-    mem = max((params["cpus"] * 3) + 2, MIN_MEM)
-    nfs = params["nfs"]
-    analysisId = params["analysisId"]
+    loop = asyncio.get_event_loop()
+    coroutines = [startVariableParamsRun(params, api_token, score_api_token) for params in paramsList]
+    return loop.run_until_complete(asyncio.gather(*coroutines))
 
-    payload = {
-        "workflow_url": "icgc-argo/nextflow-dna-seq-alignment",
-        "workflow_params": {
-            "study_id": "PANCHOPE-CA",
-            "analysis_id": analysisId,
-            "song_url": "https://song.qa.argo.cancercollaboratory.org",
-            "score_url": "https://score.qa.argo.cancercollaboratory.org",
-            "api_token": api_token,
-            "reference_dir": "/{}/reference/GRCh38_hla_decoy_ebv".format(nfs),
-            "aligned_lane_prefix": "grch38-aligned",
-            "aligned_basename": "wgs.grch38",
-            "download": {
-                "song_url": "https://intermediate-song.qa.argo.cancercollaboratory.org",
-                "song_cpus": 2,
-                "song_mem": 2,
-                "score_url": "https://storage.cancercollaboratory.org",
-                "score_api_token": score_api_token,
-                "score_cpus": 8,
-                "score_mem": 18
-            },
-            "seqtolanebam": {
-                "cpus": cpus,
-                "mem": mem
-            },
-            "align": {
-                "cpus": cpus,
-                "mem": mem
-            },
-            "merge": {
-                "cpus": 4,
-                "mem": 18
-            },
-            "sequencing_alignment_payload_gen": {
-                "cpus": 2,
-                "mem": 2
-            },
-            "upload": {
-                "song_cpus": 2,
-                "song_mem": 2,
-                "score_cpus": 8,
-                "score_mem": 18
+
+async def startVariableParamsRun(params, api_token, score_api_token, semaphore = asyncio.Semaphore(3)):
+    async with semaphore:
+        async with aiohttp.ClientSession() as session:
+            cpus = params["cpus"]
+            mem = max((params["cpus"] * 3) + 2, MIN_MEM)
+            nfs = params["nfs"]
+            analysisId = params["analysisId"]
+
+            payload = {
+                "workflow_url": "icgc-argo/nextflow-dna-seq-alignment",
+                "workflow_params": {
+                    "study_id": "PANCHOPE-CA",
+                    "analysis_id": analysisId,
+                    "song_url": "https://song.qa.argo.cancercollaboratory.org",
+                    "score_url": "https://score.qa.argo.cancercollaboratory.org",
+                    "api_token": api_token,
+                    "reference_dir": "/{}/reference/GRCh38_hla_decoy_ebv".format(nfs),
+                    "aligned_lane_prefix": "grch38-aligned",
+                    "aligned_basename": "wgs.grch38",
+                    "download": {
+                        "song_url": "https://intermediate-song.qa.argo.cancercollaboratory.org",
+                        "song_cpus": 2,
+                        "song_mem": 2,
+                        "score_url": "https://storage.cancercollaboratory.org",
+                        "score_api_token": score_api_token,
+                        "score_cpus": 8,
+                        "score_mem": 18
+                    },
+                    "seqtolanebam": {
+                        "cpus": cpus,
+                        "mem": mem
+                    },
+                    "align": {
+                        "cpus": cpus,
+                        "mem": mem
+                    },
+                    "merge": {
+                        "cpus": 4,
+                        "mem": 18
+                    },
+                    "sequencing_alignment_payload_gen": {
+                        "cpus": 2,
+                        "mem": 2
+                    },
+                    "upload": {
+                        "song_cpus": 2,
+                        "song_mem": 2,
+                        "score_cpus": 8,
+                        "score_mem": 18
+                    }
+                },
+                "workflow_engine_params": {
+                    "work_dir": "/{}/work".format(nfs)
+                }
             }
-        },
-        "workflow_engine_params": {
-            "work_dir": "/{}/work".format(nfs)
-        }
-    }
 
-    # return format for easy write into sheets as column
-    return [requests.post(WES_BASE, json=payload).json()["run_id"]]
+            async with session.post(WES_BASE, json=payload) as response:
+                data = await response.json()
+                # return format for easy write into sheets as column
+                return [data["run_id"]]
 
 
 def processTasks(task):
@@ -83,23 +115,6 @@ def processTasks(task):
             "duration": task["duration"],
             "realtime": task["realtime"]
         }
-
-
-def getWesRun(id):
-    with request.urlopen("{}{}".format(WES_BASE, id.strip())) as response:
-        data = json.loads(response.read())
-        return {
-            "runId": data["run_id"],
-            "params": data["request"]["workflow_params"],
-            "start": data["run_log"]["start_time"],
-            "end": data["run_log"]["end_time"],
-            "duration": data["run_log"]["duration"],
-            "tasks": list(filter(None, map(processTasks, data["task_logs"])))
-        }
-
-
-def getWesRuns(wesIds):
-    return map(getWesRun, wesIds)
 
 
 def extractTaskInfo(id, task):
